4c1e0396766deb0f90523e509894693f177bba80
atulbhai1 <atulbhai.nitin@gmail.com>
2022-12-21 17:23:53 -0400

    Issues Resolved Working Product

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def dividingQ(k):
     a = randint(0, 100)
     b = randint(0, 100)
     q = f"{a} ➗ {b} = "
-    ans = a/b
+    ans = a//b
     return [k,q,ans]
 def mixed():
     qs = []
@@ -63,34 +63,55 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 app = Flask(__name__)
 Bootstrap(app)
 test = "Mixed"
+data = []
+responses = {}
+score = 0
 @app.route('/', methods=('GET', 'POST'))
 def home():
-    global test
-    if request.method == 'POST' and request.form.get('GoThere') == 'GoThere':
+    global test, data
+    if request.method == 'POST':
         test = request.form.get('choice')
         print(test)
+        if test == "Addition":
+            qs = addition()
+        elif test == "Subtraction":
+            qs = subtraction()
+        elif test == "Multiplication":
+            qs = multiplying()
+        elif test == "Division":
+            qs = division()
+        else:
+            qs = mixed()
+        data = qs
         return redirect(url_for("quiz"))
     return render_template('home.html')
 
 @app.route('/quiz', methods=('GET', 'POST'))
 def quiz():
-    global test
-    if test == "Addition":
-        qs = addition()
-    elif test == "Subtraction":
-        qs = subtraction()
-    elif test == "Multiplication":
-        qs = multiplying()
-    elif test == "Division":
-        qs = division()
-    else:
-        qs = mixed()
-    data = qs
-    print(request.method)
-    if request.method == 'POST' and request.form.get('Submit') == 'Submit':
+    global data, responses, score
+    if request.method == 'POST':
         print("gotthere")
-        print(request.form['one'])
-        return redirect(url_for('home'))
-
+        responses['1'] = request.form['1']
+        responses['2'] = request.form['2']
+        responses['3'] = request.form['3']
+        responses['4'] = request.form['4']
+        responses['5'] = request.form['5']
+        responses['6'] = request.form['6']
+        responses['7'] = request.form['7']
+        responses['8'] = request.form['8']
+        responses['9'] = request.form['9']
+        responses['10'] = request.form['10']
+        t = 0
+        for d in data:
+            if int(float(responses[f'{d[0]}'])) == d[2]:
+                t += 10
+        score = t
+        return redirect(url_for('see'))
     return render_template('quiz.html', data=data)
+@app.route('/score', methods=('GET', 'POST'))
+def see():
+    global score
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    return render_template('see.html', data=score)
 app.run()
